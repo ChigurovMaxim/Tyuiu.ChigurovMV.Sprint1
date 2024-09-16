07fc715b15ec69499822f1d7f70452bd6d896286
Tyuiu.ChigurovMV@outlook.com <maxchigur@mail.ru>
2024-09-16 20:27:17 +0500

    Task1

--- Tyuiu.ChigurovMV.Sprint1.Task1.V29.Test/DataServiceTest.cs
+++ Tyuiu.ChigurovMV.Sprint1.Task1.V29.Test/DataServiceTest.cs
@@ -8,11 +8,11 @@ namespace Tyuiu.ChigurovMV.Sprint1.Task1.V29.Test
         public void ValidExpression()
         {
             DataService ds = new DataService();
-            double a = 1.0;
+            double a = 2.0;
             double b = 2.0;
-            double c = 3.0;
+            double c = 1.0;
             var res = ds.Calculate(a, b, c);
-            Assert.AreEqual(1, res);
+            Assert.AreEqual(0.8, res);
         }
     }
 }
