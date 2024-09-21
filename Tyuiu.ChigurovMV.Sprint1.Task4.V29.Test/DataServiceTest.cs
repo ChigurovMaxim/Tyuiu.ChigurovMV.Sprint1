853df63875ccc91da9402898ced448e9e02790f8
Tyuiu.ChigurovMV@outlook.com <maxchigur@mail.ru>
2024-09-21 21:50:16 +0500

    Task4

---
+++ Tyuiu.ChigurovMV.Sprint1.Task4.V29.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+using Tyuiu.ChigurovMV.Sprint1.Task4.V29.Lib;
+
+namespace Tyuiu.ChigurovMV.Sprint1.Task4.V29.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 1;
+            int y = 2;
+
+            double wait = 0.18633899812498247;
+            double res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ChigurovMV.Sprint1.Task4.V29/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.ChigurovMV.Sprint1.Task4.V29.Lib;
+
+namespace Tyuiu.ChigurovMV.Sprint1.Task4.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Чигуров М. В. | ПКТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Class Math                                                        *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #29                                                             *");
+            Console.WriteLine("* Выполнил: Чигуров Максим Владиславович | ПКТб-24-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле Sqrt(2+|x-2*y|)/3*x*y^2 и печатает       *");
+            Console.WriteLine("* его на экране.                                                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x, y;
+
+            Console.WriteLine("* Введите X:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("* Введите Y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            double res = Math.Round(ds.Calculate(x, y),3);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Sqrt(2+|x-2*y|)/3*x*y^2 = "+ res);
+        }
+    }
+}
