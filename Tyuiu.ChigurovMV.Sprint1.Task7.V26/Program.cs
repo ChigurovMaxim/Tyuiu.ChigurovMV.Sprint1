ff01d11195a8c800142eb039711e10c38ec2a442
Tyuiu.ChigurovMV@outlook.com <maxchigur@mail.ru>
2024-09-22 14:29:59 +0500

    Task7

---
+++ Tyuiu.ChigurovMV.Sprint1.Task7.V26/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.ChigurovMV.Sprint1.Task7.V26.Lib;
+
+namespace Tyuiu.ChigurovMV.Sprint1.Task7.V26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Чигуров М. В. | ПКТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #26                                                             *");
+            Console.WriteLine("* Выполнил: Чигуров Максим Владиславович | ПКТб-24-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет математическое выражение по       *");
+            Console.WriteLine("* формуле     sin(x^2) + y      x*y - 12                                  *");
+            Console.WriteLine("*         z = ------------ -  ------------                                *");
+            Console.WriteLine("*                 y + 1         34 + x^2                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x, y;
+            Console.WriteLine("* Введите X: ");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("* Введите Y: ");
+            y = Convert.ToDouble(Console.ReadLine());
+            double res = Math.Round(ds.Calculate(x, y),3);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(res);
+        }
+    }
+}
